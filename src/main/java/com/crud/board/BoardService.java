fcbdf736671915c8a13949d1c4324eac1b4f20c9
Losecow <100589396+Losecow@users.noreply.github.com>
2022-12-13 19:27:14 +0900

    fin.

---
+++ src/main/java/com/crud/board/BoardService.java
@@ -0,0 +1,14 @@
+package com.crud.board;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface BoardService {
+    public int insertBoard(BoardVO vo);
+    public int deleteBoard(int seq);
+    public int updateBoard(BoardVO vo);
+    public BoardVO getBoard(int seq);
+    public List<BoardVO> getBoardList();
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h3><a href="list">Move To List</a></h3>
+</body>
+</html>
